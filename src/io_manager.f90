795b85f208053c798100951cfc0b3623cc2be6fd
ibarraespinosa <zergioibarra@gmail.com>
2025-10-27 19:35:43 -0600

    add read_csv

--- src/io_manager.f90
+++ src/io_manager.f90
@@ -75,4 +75,149 @@ contains
     call check(status)
   end subroutine read_matrix_netcdf
 
+subroutine read_csv_obs(filename, path_col, time_col, gas_col, bg_col, &
+                          year_col, month_col, day_col, hour_col, minute_col)
+    ! Inputs
+    character(len=*), intent(in) :: filename
+    ! Outputs
+    character(len=:), allocatable, intent(out) :: path_col(:)
+    character(len=:), allocatable, intent(out) :: time_col(:)
+    real(wp), allocatable, intent(out) :: gas_col(:)
+    real(wp), allocatable, intent(out) :: bg_col(:)
+    integer(int32), allocatable, intent(out) :: year_col(:)
+    integer(int32), allocatable, intent(out) :: month_col(:)
+    integer(int32), allocatable, intent(out) :: day_col(:)
+    integer(int32), allocatable, intent(out) :: hour_col(:)
+    integer(int32), allocatable, intent(out) :: minute_col(:)
+
+    ! Local variables
+    integer :: unit_num, io_stat
+    character(len=2048) :: line_buffer
+    type(string_type) :: header_line, data_line
+    character(len=:), allocatable :: headers(:)
+    character(len=:), allocatable :: data_values(:)
+    integer :: i, num_cols
+    integer :: path_idx, time_idx, gas_idx, bg_idx, year_idx, &
+               month_idx, day_idx, hour_idx, minute_idx
+    
+    ! Temporary allocatable arrays for building output
+    character(len=512), allocatable :: path_tmp(:), time_tmp(:)
+    real(wp), allocatable :: gas_tmp(:), bg_tmp(:)
+    integer(int32), allocatable :: year_tmp(:), month_tmp(:), day_tmp(:), &
+                                   hour_tmp(:), minute_tmp(:)
+
+    ! Initialize indices to -1 (not found)
+    path_idx = -1; time_idx = -1; gas_idx = -1; bg_idx = -1; year_idx = -1; &
+    month_idx = -1; day_idx = -1; hour_idx = -1; minute_idx = -1
+
+    ! Allocate temporary arrays
+    allocate(path_tmp(0)); allocate(time_tmp(0))
+    allocate(gas_tmp(0)); allocate(bg_tmp(0))
+    allocate(year_tmp(0)); allocate(month_tmp(0)); allocate(day_tmp(0)); &
+    allocate(hour_tmp(0)); allocate(minute_tmp(0))
+
+    open(newunit=unit_num, file=filename, action="read", status="old", iostat=io_stat)
+    if (io_stat /= 0) call error_stop("Error: Could not open file: " // trim(filename))
+
+    ! 1. Read header row
+    read(unit_num, '(a)', iostat=io_stat) line_buffer
+    if (io_stat /= 0) call error_stop("Error: Could not read header from file: " // trim(filename))
+    
+    header_line = to_string(strip(line_buffer))
+    call header_line%split(',', headers)
+    num_cols = size(headers)
+
+    ! 2. Find column indices (case-insensitive)
+    do i = 1, num_cols
+      select case (to_lower(strip(headers(i))))
+        case ('path_col')
+          path_idx = i
+        case ('time_col')
+          time_idx = i
+        case ('gas_col')
+          gas_idx = i
+        case ('bg_col')
+          bg_idx = i
+        case ('year')
+          year_idx = i
+        case ('month')
+          month_idx = i
+        case ('day')
+          day_idx = i
+        case ('hour')
+          hour_idx = i
+        case ('minute')
+          minute_idx = i
+      end select
+    end do
+
+    ! 3. Check if all required columns were found
+    if (path_idx == -1) call error_stop("Error: 'path_col' not found in " // trim(filename))
+    if (time_idx == -1) call error_stop("Error: 'time_col' not found in " // trim(filename))
+    if (gas_idx == -1) call error_stop("Error: 'gas_col' not found in " // trim(filename))
+    if (bg_idx == -1) call error_stop("Error: 'bg_col' not found in " // trim(filename))
+    if (year_idx == -1) call error_stop("Error: 'year' not found in " // trim(filename))
+    if (month_idx == -1) call error_stop("Error: 'month' not found in " // trim(filename))
+    if (day_idx == -1) call error_stop("Error: 'day' not found in " // trim(filename))
+    if (hour_idx == -1) call error_stop("Error: 'hour' not found in " // trim(filename))
+    if (minute_idx == -1) call error_stop("Error: 'minute' not found in " // trim(filename))
+
+    ! 4. Read data rows
+    data_loop: do
+      read(unit_num, '(a)', iostat=io_stat) line_buffer
+      if (io_stat /= 0) exit data_loop ! Exit on end-of-file or read error
+
+      data_line = to_string(strip(line_buffer))
+      if (len(data_line) == 0) cycle data_loop ! Skip empty lines
+      
+      call data_line%split(',', data_values)
+
+      if (size(data_values) == num_cols) then
+        ! Append string data
+        path_tmp = [path_tmp, strip(data_values(path_idx))]
+        time_tmp = [time_tmp, strip(data_values(time_idx))]
+        
+        ! Append real data
+        gas_tmp = [gas_tmp, 0.0_wp] ! Allocate space
+        read(data_values(gas_idx), *) gas_tmp(size(gas_tmp))
+        
+        bg_tmp = [bg_tmp, 0.0_wp] ! Allocate space
+        read(data_values(bg_idx), *) bg_tmp(size(bg_tmp))
+
+        ! Append integer data
+        year_tmp = [year_tmp, 0_int32]
+        read(data_values(year_idx), *) year_tmp(size(year_tmp))
+        
+        month_tmp = [month_tmp, 0_int32]
+        read(data_values(month_idx), *) month_tmp(size(month_tmp))
+        
+        day_tmp = [day_tmp, 0_int32]
+        read(data_values(day_idx), *) day_tmp(size(day_tmp))
+        
+        hour_tmp = [hour_tmp, 0_int32]
+        read(data_values(hour_idx), *) hour_tmp(size(hour_tmp))
+        
+        minute_tmp = [minute_tmp, 0_int32]
+        read(data_values(minute_idx), *) minute_tmp(size(minute_tmp))
+        
+      else
+         print *, "Warning: Skipping malformed line: ", trim(line_buffer)
+      end if
+    end do data_loop
+
+    close(unit_num)
+
+    ! 5. Move data from temporary arrays to final output arrays
+    path_col = path_tmp
+    time_col = time_tmp
+    gas_col = gas_tmp
+    bg_col = bg_tmp
+    year_col = year_tmp
+    month_col = month_tmp
+    day_col = day_tmp
+    hour_col = hour_tmp
+    minute_col = minute_tmp
+
+  end subroutine read_csv_obs
+
 end module io_manager
