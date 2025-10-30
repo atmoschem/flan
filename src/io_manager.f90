6ecfef602a22ce81afe0e4c9aa10c7fe695bbf61
ibarraespinosa <zergioibarra@gmail.com>
2025-10-30 12:53:17 -0600

    refactor: update example_csv_reader to use csv-fortran for reading data

--- src/io_manager.f90
+++ src/io_manager.f90
@@ -3,6 +3,7 @@ module io_manager
   use netcdf
   use csv_module
   use stdlib_error, only: error_stop
+  use datetime_module, only:datetime
   implicit none
 
   ! CORRECT: Make both subroutines public
@@ -78,140 +79,50 @@ contains
     call check(status)
   end subroutine read_matrix_netcdf
 
-! [This code replaces the existing 'example_csv_reader' subroutine in src/io_manager.f90]
 
 ! --- Example CSV Reader using csv-fortran ---
   subroutine example_csv_reader(filename)
     character(len=*), intent(in) :: filename
     type(csv_file) :: csv
-    character(len=:), allocatable :: header(:), line_data(:)
+    character(len=30),dimension(:),allocatable :: header
     logical :: at_end
     integer :: i, ierr
     
-    ! Variables to hold column indices
-    integer :: time_idx = -1, gas_idx = -1, year_idx = -1, month_idx = -1, &
-               day_idx = -1, hour_idx = -1, minute_idx = -1, path_idx = -1
+    ! type time
+    type(datetime) :: a
 
-    ! Variables to hold data from a row
-    character(len=100) :: time_str, gas_str, path_str
-    character(len=4)   :: year_str
-    character(len=2)   :: month_str, day_str, hour_str, minute_str
-    integer :: year_int, month_int, day_int, hour_int, minute_int
+    !Variables to hold data from a row
+    character(len=100) :: path
+    real(wp),dimension(:),allocatable :: gas, bg
+    integer,dimension(:),allocatable :: year, month, day, hour, minute
     character(len=100) :: output_filename
-    
-    ! Column names to find
-    character(len=*), parameter :: TIME_COL = "time_col"
-    character(len=*), parameter :: GAS_COL = "gas_col"
-    character(len=*), parameter :: YEAR_COL = "year"
-    character(len=*), parameter :: MONTH_COL = "month"
-    character(len=*), parameter :: DAY_COL = "day"
-    character(len=*), parameter :: HOUR_COL = "hour"
-    character(len=*), parameter :: MINUTE_COL = "minute"
-    character(len=*), parameter :: PATH_COL = "path_col"
+    integer, dimension(:), allocatable :: itypes
 
-    ! Helper function to find column index
-    contains
-      function find_col_index(header_array, col_name) result(idx)
-        character(len=:), allocatable, intent(in) :: header_array(:)
-        character(len=*), intent(in) :: col_name
-        integer :: idx, j
-        idx = -1 ! Default if not found
-        do j = 1, size(header_array)
-          if (trim(header_array(j)) == col_name) then
-            idx = j
-            exit
-          end if
-        end do
-      end function find_col_index
 
-    ! --- Main Subroutine Logic ---
+    call csv%read('receptors/receptor.csv',header_row=1,status_ok=at_end)
 
-    ! Open the CSV file
-    call csv%open(filename, n_cols = 8, status_ok = at_end)
-!    if (.not. csv%is_open()) then
-!      call error_stop("Error: Could not open CSV file: " // trim(filename))
-!    end if
+    ! get the header and type info
+    call csv%get_header(header,at_end)
+    call csv%variable_types(itypes,at_end)
 
-    ! 1. Read header to find column indices
-    call read_line(csv, header, at_end)
-    if (at_end) then
-      print *, "Warning: CSV file is empty."
-      call close(csv)
-      return
-    end if
-    
-    ! Find indices for all required columns
-    time_idx   = find_col_index(header, TIME_COL)
-    gas_idx    = find_col_index(header, GAS_COL)
-    year_idx   = find_col_index(header, YEAR_COL)
-    month_idx  = find_col_index(header, MONTH_COL)
-    day_idx    = find_col_index(header, DAY_COL)
-    hour_idx   = find_col_index(header, HOUR_COL)
-    minute_idx = find_col_index(header, MINUTE_COL)
-    path_idx   = find_col_index(header, PATH_COL)
-    
-    ! Basic check if all columns were found
-    if (minval([time_idx, gas_idx, year_idx, month_idx, day_idx, hour_idx, minute_idx, path_idx]) < 1) then
-       print *, "Error: Could not find one or more required columns in CSV header."
-       print *, "Found indices: ", time_idx, gas_idx, year_idx, month_idx, day_idx, hour_idx, minute_idx, path_idx
-       call close_csv(csv)
-       call error_stop("Missing CSV columns")
-    end if
-    
-    print *, "Successfully mapped all columns."
+    call csv%get_column(1,year,at_end)
+    call csv%get(2,month,at_end)
+    call csv%get(3,day,at_end)
+    call csv%get(4,hour,at_end)
+    call csv%get(5,minute,at_end)
+    call csv%get(6,path,at_end)
+    call csv%get(7,gas,at_end)
+    call csv%get(8,bg,at_end)
 
-    ! 2. Loop through data rows
-    do
-      call read_line(csv, line_data, at_end)
-      if (at_end) exit ! Exit loop at end of file
-      
-      ! 3. Extract string data from the correct columns
-      time_str = line_data(time_idx)
-      gas_str  = line_data(gas_idx)
-      path_str = line_data(path_idx)
-      
-      ! 4. Convert numeric strings to integers for processing
-      ! Use internal read for robust string-to-integer conversion
-      read(line_data(year_idx), *, iostat=ierr) year_int
-      if (ierr /= 0) call error_stop("Error reading 'year' as integer")
-      
-      read(line_data(month_idx), *, iostat=ierr) month_int
-      if (ierr /= 0) call error_stop("Error reading 'month' as integer")
-      
-      read(line_data(day_idx), *, iostat=ierr) day_int
-      if (ierr /= 0) call error_stop("Error reading 'day' as integer")
+    call csv%destroy()
+     
+    a = datetime(year(1), month(1), day(1), hour(1), minute(1), 0) 
 
-      read(line_data(hour_idx), *, iostat=ierr) hour_int
-      if (ierr /= 0) call error_stop("Error reading 'hour' as integer")
-
-      read(line_data(minute_idx), *, iostat=ierr) minute_int
-      if (ierr /= 0) call error_stop("Error reading 'minute' as integer")
-
-      ! 5. Format integers to strings as %02d (and %04d for year)
-      ! Use internal write for robust integer-to-string formatting
-      write(year_str, "(I4.4)") year_int
-      write(month_str, "(I2.2)") month_int
-      write(day_str, "(I2.2)") day_int
-      write(hour_str, "(I2.2)") hour_int
-      write(minute_str, "(I2.2)") minute_int
-      
-      ! 6. Construct the final filename
-      output_filename = trim(year_str) // trim(month_str) // trim(day_str) &
-                      & // "_" // trim(hour_str) // trim(minute_str) // ".nc"
+    print *, a 
+    ! 6. Construct the final filename
+!      output_filename = trim(year_str) // trim(month_str) // trim(day_str) &
+!                      & // "_" // trim(hour_str) // trim(minute_str) // ".nc"
                       
-      ! --- Process the data (example: print it) ---
-      print '("--- Processing Row ---")'
-      print '("  Time: ", A, ", Gas: ", A, ", Path: ", A)', trim(time_str), trim(gas_str), trim(path_str)
-      print '("  Filename: ", A)', trim(output_filename)
-      !
-      ! You would add logic here to use 'output_filename' and 'path_str' to read the netCDF file
-      !
-      
-    end do
+    end subroutine example_csv_reader
 
-    ! Close the file
-    call close_csv(csv)
-    
-  end subroutine example_csv_reader
-  
 end module io_manager
